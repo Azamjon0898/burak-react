300abe916b0a71524b4d5d03912ac3fb8e9c1586
Azamjon0898 <98kevin08@gmail.com>
2025-01-16 20:27:10 +0900

    fix: repush

--- src/index.tsx
+++ src/index.tsx
@@ -28,5 +28,4 @@ if (container) {
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// nothing
 reportWebVitals();
