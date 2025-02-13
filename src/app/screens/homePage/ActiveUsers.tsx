559c947bce7d039dd762794669b98e71b9f959cb
Azamjon0898 <98kevin08@gmail.com>
2025-02-13 20:55:03 +0900

    fix: retreive top users and modify ActiveUsers component

--- src/app/screens/homePage/ActiveUsers.tsx
+++ src/app/screens/homePage/ActiveUsers.tsx
@@ -9,7 +9,7 @@ import { createSelector } from "reselect";
 import MemberService from "../../services/MemberService.ts";
 import { Member } from "../../../lib/types/member";
 import { useSelector } from "react-redux";
-import { serverApi } from "../../../lib/config";
+import { serverApi } from "../../../lib/config"; 
 
 // const activeUsers = [
 //   { memberNick: "Martin", memberImage: "/img/martin.webp" },
